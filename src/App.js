e61195b1b80d37318b3d6ac73105bec33350cd47
prasetyoow <praztamer@gmail.com>
2022-07-24 18:17:26 +0700

    add routing for login, signup pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home'
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import CreatePin from "./pages/CreatePin"
+import CreatePinSucess from "./pages/CreatePinSuccess";
 
 function App() {
   return (
@@ -8,7 +14,12 @@ function App() {
       <Routes>
         
         <Route path="/" element={<Home />} />
-      
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/ForgotPassword" element={<ForgotPassword />} />
+        <Route path="/ResetPassword" element={<ResetPassword />} />
+        <Route path="/CreatePin" element={<CreatePin />} />
+        <Route path="/CreatePinSuccess" element={<CreatePinSucess />} />
       </Routes>
     
   )
