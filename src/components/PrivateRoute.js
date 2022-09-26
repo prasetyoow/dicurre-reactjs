a9602f25ad719e4233a91f7f47db97428d825cc4
prasetyoow <praztamer@gmail.com>
2022-09-26 12:06:36 +0700

    fixed private route (token usage)

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,13 +1,15 @@
+import React from "react";
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({ children }) => {
-  const auth = localStorage.getItem("auth");
 
-  return auth ? (
-    children
-  ) : (
-    <Navigate to="/Login" replace={true} state={{ errorMsg: "You must login to acces this page"}} />
-  );
+const PrivateRoute = ({ children }) => {
+  const token = useSelector((state) => state.auth.token);
+
+  if (token) {
+    return children;
+  }
+  return <Navigate to={"/Login"} />;
 };
 
 export default PrivateRoute;
