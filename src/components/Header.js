029808193074aac3e6da55cb035e3c98485909cb
prasetyoow <praztamer@gmail.com>
2022-10-04 18:33:34 +0700

    fixed get data user login

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Row, Col } from "react-bootstrap"
 import { Link } from 'react-router-dom';
 import {FiBell} from "react-icons/fi"
-import profDef from "../assets/img/defaultProfile.png"
+// import profDef from "../assets/img/defaultProfile.png"
 
 // redux
 import {getUserLogin} from "../redux/asyncActions/profile"
@@ -12,6 +12,7 @@ function Header() {
   const dispatch = useDispatch();
   const profile = useSelector(state => state.profile.data)
   const token = useSelector(state => state.auth.token)
+  console.log(profile.picture + ' ini picture')
   React.useEffect(() => {
     dispatch(getUserLogin(token));
   }, [dispatch, token]);
@@ -29,7 +30,7 @@ function Header() {
             <div className="d-flex align-items-center justify-content-end p-5">
                <Link to="/Profile" className="text-decoration-none text-muted">
                  <div className="profile-head">
-                   <img className="header-prof-pict" src={profDef} alt="head-profile"/>
+                   <img className="header-prof-pict" src={'http://192.168.1.10:8787/public/uploads/' + profile.picture} alt="head-profile"/>
                  </div>
                </Link>
               
